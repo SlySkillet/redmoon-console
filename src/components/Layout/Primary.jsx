da0d8018bfe574d544d8b9cd9841e9ee3fa28294
Simon Conrad <simondbconrad@gmail.com>
2023-09-19 15:48:49 -0400

    bring in redmoon logo

--- src/components/Layout/Primary.jsx
+++ src/components/Layout/Primary.jsx
@@ -1,5 +1,15 @@
 const Primary = () => {
-  return <div className="primary">working</div>;
+  return (
+    <div className="primary">
+      <img
+        className="redmoon-logo"
+        alt="redmoon"
+        src={
+          "https://res.cloudinary.com/ddgt67wcb/image/upload/v1695152640/redmoon-logo_nyyrt6.jpg"
+        }
+      />
+    </div>
+  );
 };
 
 export default Primary;
