4944029921fea56b6953db1ebad833ac2e2c7f80
Simon Conrad <simondbconrad@gmail.com>
2023-09-19 16:43:26 -0400

    add quote to secondary

--- src/components/Layout/Secondary.jsx
+++ src/components/Layout/Secondary.jsx
@@ -1,5 +1,13 @@
 const Secondary = () => {
-  return <div className="secondary">working</div>;
+  return (
+    <div className="secondary">
+      <p className="quote">
+        {" "}
+        "A Software Apprenticeship Program for developers, by developers. We
+        provide junior software engineer candidates that are a cut above."{" "}
+      </p>
+    </div>
+  );
 };
 
 export default Secondary;
