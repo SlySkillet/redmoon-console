67c546c0cbb73e5f72637fcc10f7ffb7820c9f26
Simon Conrad <simondbconrad@gmail.com>
2023-09-19 17:19:11 -0400

    finish preliminary cards design

--- src/components/Layout/Secondary.jsx
+++ src/components/Layout/Secondary.jsx
@@ -6,6 +6,40 @@ const Secondary = () => {
         "A Software Apprenticeship Program for developers, by developers. We
         provide junior software engineer candidates that are a cut above."{" "}
       </p>
+      <div className="card student-card">
+        <h2 className="card-title">Students</h2>
+        <img
+          className="card-image"
+          alt="image"
+          src={
+            "https://res.cloudinary.com/ddgt67wcb/image/upload/v1694186002/samples/people/bicycle.jpg"
+          }
+        />
+        <p className="card-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
+          labore, officia, unde illo vel odit veniam maxime aperiam enim
+          laboriosam soluta quisquam? Assumenda odio animi aut impedit ea. Cum,
+          fuga.
+        </p>
+        <button className="card-link">Link</button>
+      </div>
+      <div className="card employer-card">
+        <h2 className="card-title">Employers</h2>
+        <img
+          className="card-image"
+          alt="image"
+          src={
+            "https://res.cloudinary.com/ddgt67wcb/image/upload/v1694186001/samples/animals/three-dogs.jpg"
+          }
+        />
+        <p className="card-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
+          labore, officia, unde illo vel odit veniam maxime aperiam enim
+          laboriosam soluta quisquam? Assumenda odio animi aut impedit ea. Cum,
+          fuga.
+        </p>
+        <button className="card-link">Link</button>
+      </div>
     </div>
   );
 };
