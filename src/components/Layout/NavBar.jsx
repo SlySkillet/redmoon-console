27d8041b2b711a14c7cf0982bbb59a073f776996
Simon Conrad <simondbconrad@gmail.com>
2023-09-19 15:31:54 -0400

    add some example links to navbar

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './layout.css';
+import React from "react";
+import "./layout.css";
 
 const NavBar = () => {
-
-
   return (
     <nav>
-        
+      <a>Login</a>
+      <a>About</a>
+      <a>Apprentices</a>
+      <a>Join</a>
     </nav>
   );
 };
